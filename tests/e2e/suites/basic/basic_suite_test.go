5d7096b19643404ad31bcda9dba942a67b5c5032
Franco <franco@giantswarm.io>
2024-05-09 21:08:25 -0300

    Update tests

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -7,6 +7,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/giantswarm/apptest-framework/pkg/config"
 	"github.com/giantswarm/apptest-framework/pkg/state"
@@ -31,17 +33,26 @@ func TestBasic(t *testing.T) {
 
 	suite.New(config.MustLoad("../../config.yaml")).
 		// The namespace to install the app into within the workload cluster
-		WithInstallNamespace(bundleNamespace).
+		WithInstallNamespace(state.GetCluster().Organization.GetNamespace()).
 		// If this is an upgrade test or not.
 		// If true, the suite will first install the latest released version of the app before upgrading to the test version
 		WithIsUpgrade(isUpgrade).
 		WithValuesFile("./values.yaml").
-		AfterClusterReady(func() {
-			It("should have kyverno and kyverno-policy-operator deplyoed", func() {
-				org := state.GetCluster().Organization
+		BeforeUpgrade(func() {
+			// Perform any checks between installing the latest released version
+			// and upgrading it to the version to test
+			// E.g. ensure that the initial install has completed and has settled before upgrading
+		}).
+		Tests(func() {
+			org := state.GetCluster().Organization
 
-				// Ingress-Nginx depends on external-dns for DNS resolution and cert-manager for certificates.
+			It("should have kyverno, kyverno-policies and kyverno-policy-operator deplyoed", func() {
 				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-kyverno", state.GetCluster().Name), org.GetNamespace())).
+					WithTimeout(appReadyTimeout).
+					WithPolling(appReadyInterval).
+					Should(BeTrue())
+
+				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-kyverno-policies", state.GetCluster().Name), org.GetNamespace())).
 					WithTimeout(appReadyTimeout).
 					WithPolling(appReadyInterval).
 					Should(BeTrue())
@@ -51,23 +62,59 @@ func TestBasic(t *testing.T) {
 					WithPolling(appReadyInterval).
 					Should(BeTrue())
 			})
-		}).
-		BeforeUpgrade(func() {
-			// Perform any checks between installing the latest released version
-			// and upgrading it to the version to test
-			// E.g. ensure that the initial install has completed and has settled before upgrading
-		}).
-		Tests(func() {
-			// kyvernoAdmissionDeploymentName := "kyverno-admission-controller"
-			// kyvernoAdmissionDeployment := v1.Deployment{}
 
-			// kyvernoDeploymentLookup := types.NamespacedName{Name: kyvernoAdmissionDeploymentName, Namespace: kyvernoNamespace}
+			It("should have trivy, trivy-operator and starboard-exporter deplyoed", func() {
+				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-trivy", state.GetCluster().Name), org.GetNamespace())).
+					WithTimeout(appReadyTimeout).
+					WithPolling(appReadyInterval).
+					Should(BeTrue())
+
+				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-trivy-operator", state.GetCluster().Name), org.GetNamespace())).
+					WithTimeout(appReadyTimeout).
+					WithPolling(appReadyInterval).
+					Should(BeTrue())
+
+				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-starboard-exporter", state.GetCluster().Name), org.GetNamespace())).
+					WithTimeout(appReadyTimeout).
+					WithPolling(appReadyInterval).
+					Should(BeTrue())
+			})
+
+			It("should have falco deplyoed", func() {
+				Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-falco", state.GetCluster().Name), org.GetNamespace())).
+					WithTimeout(appReadyTimeout).
+					WithPolling(appReadyInterval).
+					Should(BeTrue())
+			})
+
+			kyvernoAdmissionDeploymentName := "kyverno-admission-controller"
+			kyvernoAdmissionDeployment := v1.Deployment{}
+
+			kyvernoDeploymentLookup := types.NamespacedName{Name: kyvernoAdmissionDeploymentName, Namespace: kyvernoNamespace}
 
 			It("should have kyverno running", func() {
-				// STEP
-				By("checking if the deployment is satisfied")
+				By("checking if the kyverno-admission-controller Deployment is satisfied")
+				clusterName := state.GetCluster().Name
+
 				Eventually(func() bool {
-					return true
+					wcClient, err := state.GetFramework().WC(clusterName)
+					if err != nil {
+						fmt.Println("Unable to get Workload Cluster client")
+						return false
+					}
+					err = wcClient.Get(state.GetContext(), kyvernoDeploymentLookup, &kyvernoAdmissionDeployment)
+					if err != nil {
+						fmt.Printf("Unable to get %s Deployment", kyvernoAdmissionDeploymentName)
+						return false
+					}
+
+					if kyvernoAdmissionDeployment.Status.ReadyReplicas >= 3 {
+						fmt.Printf("%s Deployment has %d replicas ready", kyvernoAdmissionDeploymentName, kyvernoAdmissionDeployment.Status.ReadyReplicas)
+						return true
+					} else {
+						fmt.Printf("%s Deployment is not yet satisfied: Has %d replicas ready", kyvernoAdmissionDeploymentName, kyvernoAdmissionDeployment.Status.ReadyReplicas)
+						return false
+					}
 				}, timeout, interval).Should(BeTrue())
 			})
 		}).
